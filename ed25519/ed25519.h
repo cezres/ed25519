b822e30573154240aa046861079d7051263dd4e8
SuperY <yuanlinyue@gmail.com>
2018-11-14 14:14:45 +0800

    Initial Commit

---
+++ ed25519/ed25519.h
@@ -0,0 +1,19 @@
+//
+//  ed25519.h
+//  ed25519
+//
+//  Created by Felix Yuan on 2018/11/14.
+//  Copyright © 2018 SuperY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ed25519.
+FOUNDATION_EXPORT double ed25519VersionNumber;
+
+//! Project version string for ed25519.
+FOUNDATION_EXPORT const unsigned char ed25519VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ed25519/PublicHeader.h>
+
+
